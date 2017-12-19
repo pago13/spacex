86637f35bd7b2683a67a614586d7751c9e2510b5
Ryzlane <ryzlane@free.fr>
2017-12-19 16:35:07 +0100

    fix stars and global timeline

---
+++ scripts/oldStars.js
@@ -0,0 +1,72 @@
+const $canvas = document.querySelector('canvas')
+const context = $canvas.getContext('2d')
+const numberParticle = 300
+const stars = []
+
+/* RESIZE */ 
+const resize = () =>
+{
+  $canvas.width = window.innerWidth-2
+  $canvas.height = window.innerHeight-2
+}
+
+  window.addEventListener('resize', resize)
+  resize()
+
+/* SPACE */ 
+const space = () =>
+{
+  context.beginPath() 
+  context.rect(0, 0, $canvas.width, ($canvas.height))
+  context.fillStyle = ' rgba(11, 11, 11, 1)'
+  context.fill()
+
+}
+
+const starsFunction = () =>
+{
+  for (let i = 0; i < numberParticle*2; i++) 
+  {
+    context.fillStyle= ' rgba(255, 255, 255, 1)'
+    let x = (Math.random() * ($canvas.width))
+    let y = (Math.random() * ($canvas.height))
+    stars[i]= x
+    stars[i+1]= y
+    context.beginPath()
+    context.arc(x, y, 1, 0, Math.PI*2)
+    context.fill()
+    i++
+  }
+}
+starsFunction()
+
+const starsFunctionAnim = () =>
+{
+  for (let i = 0; i < numberParticle*2; i++) 
+  {
+    if(stars[i+1] >= ($canvas.height)-2)
+    {
+      stars[i+1] = 0
+    }
+    context.fillStyle= ' rgba(255, 255, 255, 1)'
+    let x = stars[i] 
+    let y = stars[i+1] + 0.2  
+    stars[i]= x
+    stars[i+1]= y
+    context.beginPath()
+    context.arc(x, y, 1, 0, Math.PI*2)
+    context.fill()
+    i++
+  }
+}
+
+
+const loop = () =>
+{
+  window.requestAnimationFrame(loop)
+  space()
+  starsFunctionAnim()
+}
+
+loop()
+
--- scripts/stars.js
+++ scripts/stars.js
@@ -3,11 +3,12 @@ const context = $canvas.getContext('2d')
 const numberParticle = 300
 const stars = []
 
+
 /* RESIZE */ 
 const resize = () =>
 {
-  $canvas.width = window.innerWidth-2
-  $canvas.height = window.innerHeight-2
+  $canvas.width = window.innerWidth -2
+  $canvas.height = window.innerHeight -2  //13800
 }
 
   window.addEventListener('resize', resize)
@@ -27,15 +28,19 @@ const starsFunction = () =>
 {
   for (let i = 0; i < numberParticle*2; i++) 
   {
-    context.fillStyle= ' rgba(255, 255, 255, 1)'
+    context.fillStyle= ' rgba(255, 255, 255, 0.5)'
     let x = (Math.random() * ($canvas.width))
     let y = (Math.random() * ($canvas.height))
     stars[i]= x
     stars[i+1]= y
+    let speedRandom = Math.random()*0.5
+    let sizeRandom = Math.random()*3
+    stars[i+2]= speedRandom
+    stars[i+3]= sizeRandom
     context.beginPath()
-    context.arc(x, y, 1, 0, Math.PI*2)
+    context.arc(x, y, sizeRandom, 0, Math.PI*2)
     context.fill()
-    i++
+    i += 3
   }
 }
 starsFunction()
@@ -44,28 +49,108 @@ const starsFunctionAnim = () =>
 {
   for (let i = 0; i < numberParticle*2; i++) 
   {
-    if(stars[i+1] >= ($canvas.height)-2)
+    if(stars[i+1] >= ($canvas.height))
     {
       stars[i+1] = 0
     }
-    context.fillStyle= ' rgba(255, 255, 255, 1)'
+    context.fillStyle= ' rgba(255, 255, 255, 0.5)'
     let x = stars[i] 
-    let y = stars[i+1] + 0.2  
+    let y = stars[i+1] + stars[i+2]
     stars[i]= x
     stars[i+1]= y
     context.beginPath()
-    context.arc(x, y, 1, 0, Math.PI*2)
+    context.arc(x, y, stars[i+3], 0, Math.PI*2)
     context.fill()
-    i++
+    i += 3
   }
 }
 
 
+
+
+const tell = () => {
+function handleStart(evt) {
+  evt.preventDefault();
+  var touches = evt.changedTouches;
+        
+  for (var i=0; i<touches.length; i++) {
+    ongoingTouches.push(touches[i]);
+    var color = colorForTouch(touches[i]);
+    zoom()  
+  }
+}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+const ocean = () => 
+{
+  context.beginPath()
+  context.fillStyle = ' rgb(0, 141, 223)'
+  context.rect(0, $canvas.height-100, $canvas.width, 100  )
+  context.fill()
+  
+}
+
+/*var derniere_position_de_scroll_connue = 0;
+var ticking = false;
+
+function faitQuelquechose(position_scroll) {
+  // faites quelque chose avec la position du scroll
+  console.log('teststststst')
+}
+
+window.addEventListener('scroll', function(e) {
+  derniere_position_de_scroll_connue = window.scrollY;
+  if (!ticking) {
+    window.requestAnimationFrame(function() {
+      if
+      faitQuelquechose(derniere_position_de_scroll_connue);
+      ticking = false;
+    });
+  }
+  ticking = true;
+}); */
+
+/* scrool */ 
+let currentZoom = 0
+let mouse = 0
+
+const zoom = (e) => {
+  if ( e.deltaY > 0 && mouse <= 100) 
+  {
+    console.log('bas')
+    mouse++
+    
+  } else if ( e.deltaY < 0 && mouse >= 0) 
+  {
+    console.log('haut')
+    mouse--
+  }
+}
+// Firefox
+document.addEventListener("DOMMouseScroll", zoom)
+// Chrome based navigator
+document.addEventListener("mousewheel", zoom)
+
+/*                     */
+  
 const loop = () =>
 {
   window.requestAnimationFrame(loop)
   space()
   starsFunctionAnim()
+  //ocean()
+ // tell()
 }
 
 loop()
